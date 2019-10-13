57355b01c1e9f453e4fa0bd6736fa103c33042a4
Ritik Soni <34769340+ritik07@users.noreply.github.com>
2019-10-13 11:51:50 +0530

    fixed bug coming up for notification sound

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(compress());
 app.use(express.static('build'));
 app.use(express.static('static'));
 
-var title = "Pomodoro app"
+
+var title = "Pomodoro app notification sound"
 var helloMessage = "Hey, there! Welcome to Pomodoro app!";
 
 app.get('/', function(req, res){
@@ -21,3 +22,47 @@ app.get('/', function(req, res){
 var server = app.listen(PORT, function(){
   console.log("App is running on port %s", PORT);
 })
+
+const got = require('got');
+const moment = require('moment');
+const loopSeconds = 120;
+const myLoc = { latitude: 32.715738, longitude: -117.161084 };
+const url = `http://api.open-notify.org/iss-pass.json?lat=${myLoc.latitude}&lon=${myLoc.longitude}`;
+const warningMinutes = 30;
+let providedNotification = false;
+let alarmAcknowledged = false;
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+        process.exit();
+    } else if (key.name === 'a') {
+        if (!alarmAcknowledged) {
+            console.log('alarm acknowledged');
+        }
+        alarmAcknowledged = true;
+    }
+});
+function sendNotification(message) {
+    console.log(`Could send email here: ${message}`);
+}
+function soundAlarm() {
+    console.log('playing sound');
+}
+function loop() {
+    got(url, { json: true })
+        .then(iss => {
+            const nextPasses = iss.body.response;
+            const now = moment();
+            const pass = nextPasses[0];
+            const passTime = moment.unix(pass.risetime);
+            const timeFromNow = moment.duration(passTime.diff(now));
+            const minutesFromNow = Number(timeFromNow.asMinutes()).toFixed(1);
+            console.log(`${passTime} (in ${minutesFromNow} minutes) for ${pass.duration} seconds`);
+            if (minutesFromNow <= warningMinutes) { if (!alarmAcknowledged) { soundAlarm(); } if (!providedNotification) { sendNotification(`The ISS will pass over in ${minutesFromNow} minutes`); providedNotification = true; } } else { providedNotification = false; alarmAcknowledged = false; } }) .catch(error => {
+            console.log(error.response.body);
+        });
+    setTimeout(loop, loopSeconds * 1000);
+}
+loop();
